9ad6628634c0dafc65583ccebbb9ab658f20f762
omarloren <omarloren@gmail.com>
2014-01-20 09:50:58 -0600

    encuesta no nav && head

--- app/views/layout/nav.blade.php
+++ app/views/layout/nav.blade.php
@@ -1,13 +1,14 @@
-
-<nav id="sidebar" class="sidebar nav-collapse collapse">
-    <ul id="side-nav" class="side-nav">
-        <li class="{{ Request::is( '/') ? 'active' : '' }}">
-            <a href="/"><i class="fa fa-home"></i> <span class="name">Inicio</span></a>
-        </li>
-    </ul>
-    <ul id="side-nav" class="side-nav">
-        <li class="{{ Request::is( 'admin/usuarios') ? 'active' : '' }}">
-            <a href="/admin/usuarios"><i class="fa fa-group"></i> <span class="name">Usuarios</span></a>
-        </li>
-    </ul>
-</nav>
+@if (!Request::is( 'encuesta'))
+    <nav id="sidebar" class="sidebar nav-collapse collapse">
+        <ul id="side-nav" class="side-nav">
+            <li class="{{ Request::is( '/') ? 'active' : '' }}">
+                <a href="/"><i class="fa fa-home"></i> <span class="name">Inicio</span></a>
+            </li>
+        </ul>
+        <ul id="side-nav" class="side-nav">
+            <li class="{{ Request::is( 'admin/usuarios') ? 'active' : '' }}">
+                <a href="/admin/usuarios"><i class="fa fa-group"></i> <span class="name">Usuarios</span></a>
+            </li>
+        </ul>
+    </nav>
+@endif
